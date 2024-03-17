12c2e861f5b72e18fd65aa1003b2b20b9cd3150c
leeyiwan <leeyiwan2001@gmail.com>
2024-03-17 18:52:06 +0800

    add tngIGC achievement

--- src/views/achivements.jsx
+++ src/views/achivements.jsx
@@ -18,15 +18,17 @@ function Achievements() {
               Here are the list of competitions and volunteering activities I have participated in:
               <div className="mt-12 grid grid-cols-1 2xl:grid-cols-3 lg:grid-cols-2 gap-x-12 gap-y-12 items-start">
                 <AchievementCard title={"Dell Hack2Hire 2023"} description={"A 2-days hackathon based hiring event organized by Dell Technologies."} year={"2023"} award={"Honorable Mention"} />
+                <AchievementCard title={"Touch 'n Go Innovative Group Challenge"} description={"1-month competition to brainstorm and provide innovative solutions to the given problem statement."} year={"2023"} award={"Runner-up"} />
                 <AchievementCard title={"UM Hackathon 2023"} description={"A 3-days competition-style events organized by Persatuan Komputer Universiti Malaya (PEKOM) and Faculty of Computer Science and Information System."} year={"2023"} award={"2nd-Runner Up (Open Category)"} />
-                <AchievementCard title={"Programming League National 2023"} description={"An annual competitive programming contest held by Computer Society of University Malaya (PEKOM) where a team of 3 will compete to solve the algorithmic problem in a given time."} year={"2023"} award={"Finalist (Open Category)"} />
 
+                <AchievementCard title={"Programming League National 2023"} description={"An annual competitive programming contest held by Computer Society of University Malaya (PEKOM) where a team of 3 will compete to solve the algorithmic problem in a given time."} year={"2023"} award={"Finalist (Open Category)"} />
                 <AchievementCard title={"Shopee Code League 2022"} description={"A 2-week coding league consisting of two rounds of coding competitions to solve a series of challenging algorithmic puzzles."} year={"2022"} award={"Participant"} />
                 <AchievementCard title={"Code Nection 2022"} description={"A competitive programming competition organized by IT Society MMU Cyberjaya. Participants will tackle challenging coding problems under tight time constraints."} year={"2022"} award={"Participant"} />
-                <AchievementCard title={"Programming League National 2022"} description={"An annual competitive programming contest held by Computer Society of University Malaya (PEKOM) where a team of 3 will compete to solve the algorithmic problem in a given time."} year={"2022"} award={"Finalist (Open Category)"} />
                 
+                <AchievementCard title={"Programming League National 2022"} description={"An annual competitive programming contest held by Computer Society of University Malaya (PEKOM) where a team of 3 will compete to solve the algorithmic problem in a given time."} year={"2022"} award={"Finalist (Open Category)"} />
                 <AchievementCard title={"Shopee Code League 2021"} description={"The largest code-from-home League in Southeast Asia and Taiwan, striving in three virtual competitions across data science, data analytics, and algorithms."} year={"2021"} award={"Participant (Student Category)"} />
                 <AchievementCard title={"Code Nection 2021"} description={"A competitive programming competition organized by IT Society MMU Cyberjaya. Participants will tackle challenging coding problems under tight time constraints."} year={"2021"} award={"Participant (Open Category)"} />
+                
                 <AchievementCard title={"Programming League National 2021"} description={"An annual competitive programming contest held by Computer Society of University Malaya (PEKOM) where a team of 3 will compete to solve the algorithmic problem in a given time."} year={"2021"} award={"Finalist (Closed Category)"} />
               </div>
             </p>
